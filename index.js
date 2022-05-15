97ba39daeb11e2834dd3be13c051e8797e120767
kiniun1 <levi.marcos17@hotmail.com>
2022-05-14 22:06:30 -0300

    refatoração index.js que inicia o projeto adicionado o mongo

--- index.js
+++ index.js
@@ -2,11 +2,15 @@ const express = require('express');
 const app = express();
 const router = require('./interface/routes/users-route');
 const buildingDataBase = require('./infrastructure/database/building-modeling-structure');
+const MongoHelper = require('./infrastructure/helpers/mongo-helper');
+
 buildingDataBase.sequelize.sync();
+app.use(express.json());
+app.use(router);
+MongoHelper.connect()
+.then(() => {
+    app.listen(3001, ()=> console.log("Server funcionando na porta 3001"));
+})
+.catch(console.error)
 
-app.use(express.json())
-app.use(router)
-
-
-app.listen(3001, ()=> console.log("Server funcionando na porta 3001"));
 module.exports = app;
