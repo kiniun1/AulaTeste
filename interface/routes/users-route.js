9b8e1d249c5b93237499c56b1e674a91143fb415
kiniun1 <levi.marcos17@hotmail.com>
2022-05-14 22:08:29 -0300

    route file updated with controller calls and http responses

--- interface/routes/users-route.js
+++ interface/routes/users-route.js
@@ -1,16 +1,19 @@
 const express = require('express');
 const router = express.Router();
-const userGet = require('../../infrastructure/repository/user-repository/user-registration-respository');
-const userSave = require('../../infrastructure/save-user');
+const userSaveController = require('../../applications/user-save-controller');
+const userUpdateController = require('../../applications/user-update-controller');
+const userGetPostgres = require('../../infrastructure/repository/user-repository/user-registration-respository');
+const httpStatusResponse = require('../../commons/http-response/http-status-response');
  
 router
 .post("/users", async (req, res) => {
     try {
         console.log('Rota POST users');
-        await userSave(req.body);
-        res.status(200).send('Salvo com sucesso');
+        const resultController = await userSaveController(req.body);
+        res.status(resultController.statusCode).send(resultController.body);
     } catch (error) {
-        res.send(error)           
+        const finalError = await httpStatusResponse(500, (error.message), 'usersRoute');
+        res.status(finalError.statusCode).send(finalError.body);         
     }  
 });
 
@@ -18,10 +21,22 @@ router
 .get("/users", async (req, res) => {
     try {
         console.log('Rota GET users');
-        const resultQuery = await userGet();
+        const resultQuery = await userGetPostgres();
         res.status(200).send(resultQuery);
     } catch (error) {
         res.send(error)           
+    }  
+});
+
+router
+.put("/users", async (req, res) => {
+    try {
+        console.log('Rota PUT users');
+        const resultController = await userUpdateController(req.body);
+        res.status(resultController.statusCode).send(resultController.body);
+    } catch (error) {
+        const finalError = await httpStatusResponse(500, (error.message), 'usersRoute');
+        res.status(finalError.statusCode).send(finalError.body);         
     }  
 });
  
